259c65587ec2e9309de4e8dee44c6b1eaf354628
alfonso <alfonso@zipcodes-mbp.myfiosgateway.com>
2020-11-01 14:45:39 -0500

    additional scientific features - AG

--- calculator.py
+++ calculator.py
@@ -1,12 +1,86 @@
+import math
+
 class Calculator:
 
     def __init__(self):
         pass
 
-    def add(self, x, y):
-        return x + y
+    def add(self, x, y, disp_mode):
+        answer = int(x+y)
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
 
-    def sub(self, x, y):
-        return 0
+    # def add(self, x, y):
+    #     return x + y
 
-# add lots more methods to this calculator class.
+    def sub(self, x, y, disp_mode):
+        answer = int(x-y)
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
+    def mul(self, x, y, disp_mode):
+        answer = int(x * y)
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
+
+    def div(self, x, y, disp_mode):
+        answer = int(x / y)
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
+
+    def squ(self, x, disp_mode):
+        answer = int(x ** x)
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
+
+    def roo(self, x, disp_mode):
+        answer = int(math.sqrt(x))
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
+
+    def exp(self, x, y, disp_mode):
+        answer = int(x ** y)
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
+
--- main-app.py
+++ main-app.py
@@ -1,26 +1,203 @@
 from calculator import Calculator
+display_modes = ['decimal', 'binary', 'octal', 'hexadecimal']
+val_in_mem = 0
+def calcMenu():
+    print("""
+        TEAM COCOA CALCULATOR PROGRAM
+    TODAYS SPECIALS: 
+    1) ADD
+    2) SUBTRACT
+    3) MULTIPLY
+    4) DIVIDE
+    5) SQUARE
+    6) SQUARE ROOT
+    7) EXPONENTS
+    8) EXIT
+    """)
+    choice = int(input("PLEASE ENTER A NUMBER BETWEEN 1 - 8: \n"))
+    return choice
 
+def getAddNum():
+    global val_in_mem
+    a = input("Enter the first number that you want to add: \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
+    b = input("Enter the second number that you want to add: \n")
+    if b == 'mrc':
+        b = float(val_in_mem)
+    else:
+        b = float(b)
 
-def getTwoNumbers():
-    a = float(input("first number? "))
-    b = float(input("second number? "))
     return a, b
+
+def getSubNum():
+    global val_in_mem
+    a = input("Enter the first number that you want to subtract: \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
+    b = input("Enter the second number that you want to subtract: \n")
+    if b == 'mrc':
+        b = float(val_in_mem)
+    else:
+        b = float(b)
+    return a, b
+
+def getMultiNum():
+    global val_in_mem
+    a = input("Enter the first number that you want to multiply: \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
+    b = input("Enter the second number that you want to multiply: \n")
+    if b == 'mrc':
+        b = float(val_in_mem)
+    else:
+        b = float(b)
+    return a, b
+
+def getDivNum():
+    global val_in_mem
+    a = input("Enter the first number that you want to divide: \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
+    b = input("Enter the second number that you want to divide: \n")
+    if b == 'mrc':
+        b = float(val_in_mem)
+    else:
+        b = float(b)
+    return a, b
+
+def getSquared():
+    global val_in_mem
+    a = input("Enter the number that you want to square: \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
+    return a
+
+def getRoot():
+    global val_in_mem
+    a = input("Enter the number that you want to find the square root of : \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
+    return a
+
+def getExponent():
+    global val_in_mem
+    a = input("Enter the base number : \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
+    b = input("Enter the exponent number : \n")
+    if b == 'mrc':
+        b = float(val_in_mem)
+    else:
+        b = float(b)
+    return a, b
+
+def switchDisplayMode(user_choice, prev_mode):
+    if user_choice == 'default':
+        disp_mode = display_modes[prev_mode]
+    else:
+        disp_mode = user_choice
+    return disp_mode
+
+
+# def getTwoNumbers():
+#     a = float(input("first number? "))
+#     b = float(input("second number? "))
+#     return a, b
 
 
 def displayResult(x: float):
-    print(x, "\n")
+    print(f"Output: {x}", "\n")
 
+
+# def performCalcLoop(calc):
+#     while True:
+#         choice = input("Operation? ")
+#         if choice == 'q':
+#             break  # user types q to quit calulator.
+#         elif choice == 'add':
+#             a, b = getAddNum()
+#             displayResult(calc.add(a, b))
+#         else:
+#             print("That is not a valid input.")
 
 def performCalcLoop(calc):
-    while True:
-        choice = input("Operation? ")
-        if choice == 'q':
-            break  # user types q to quit calulator.
-        elif choice == 'add':
-            a, b = getTwoNumbers()
-            displayResult(calc.add(a, b))
+    global val_in_mem
+    disp_prev_num = 0
+    disp_ask = input("Set a display mode?\n")
+    if disp_ask == '' or disp_ask == 'n':
+        disp_inpt = 'default'
+    elif disp_ask == 'y':
+        disp_inpt = input("Which mode" + str(display_modes))
+    choice = "" #Set choice to a value so it can meet the condition below and start the loop
+    while choice != 8:
+        if disp_prev_num > 3:
+            disp_prev_num = 0
+        disp_mode = switchDisplayMode(disp_inpt, disp_prev_num)
+        choice = calcMenu()
+        if choice == 1:
+            print("Add")
+            a, b = getAddNum()
+            result = calc.add(a, b, disp_mode)
+            displayResult(result)
+        elif choice == 2:
+            print("Subtract")
+            a, b = getSubNum()
+            displayResult(calc.sub(a, b, disp_mode))
+        elif choice == 3:
+            print("Multiply")
+            a, b = getMultiNum()
+            displayResult(calc.mul(a, b, disp_mode))
+        elif choice == 4:
+            print("Divide")
+            a, b = getDivNum()
+            displayResult(calc.div(a, b, disp_mode))
+        elif choice == 5:
+            print("Square")
+            a, b = getSquared()
+            displayResult(calc.squ(a, b, disp_mode))
+        elif choice == 6:
+            print("Square Root")
+            a = getRoot()
+            displayResult(calc.roo(a, disp_mode))
+        elif choice == 7:
+            print("Exponent")
+            a, b = getExponent()
+            displayResult(calc.exp(a, b, disp_mode))
+        elif choice == 8:
+            print("Calculator is now OFF")
         else:
-            print("That is not a valid input.")
+            print("Invalid choice: Please enter a number between 1 - 8\n\n")
+        disp_prev_num += 1
+        user_inpt = input()
+        if user_inpt == 'm+':
+            val_in_mem = result
+            print(result)
+        if user_inpt == 'mc':
+            val_in_mem = 0
+# main start
+def main():
+    calc = Calculator()
+    performCalcLoop(calc)
+    print("Done Calculating.")
+
+
+if __name__ == '__main__':
+    main()
 
 
 # main start
