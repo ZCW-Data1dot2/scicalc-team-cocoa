3b061ac41e831fa0368b60a3361626171b22ae65
sheng <itzshengh95@gmail.com>
2020-11-01 23:07:29 -0500

    Updated functions and features

--- calculator.py
+++ calculator.py
@@ -1,32 +1,97 @@
 import math
-import matplotlib as plt
+#import matplotlib.pyplot
+
 
 class Calculator:
 
     def __init__(self):
         pass
 
-    def add(self, x, y):
-        return x + y
+    def add(self, x, y, disp_mode):
+        answer = int(x + y)
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
 
-    def sub(self, x, y):
-        return 0
+    # def add(self, x, y):
+    #     return x + y
 
-    def factorial(self, x):
-        start = 1
-        for num in range(2, n + 1):
-            start *= num
-        return start
+    def sub(self, x, y, disp_mode):
+        answer = int(x - y)
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
+
+    def mul(self, x, y, disp_mode):
+        answer = int(x * y)
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
+
+    def div(self, x, y, disp_mode):
+        answer = int(x / y)
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
+
+    def squ(self, x, disp_mode):
+        answer = int(x ** x)
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
+
+    def roo(self, x, disp_mode):
+        answer = int(math.sqrt(x))
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
+
+    def exp(self, x, y, disp_mode):
+        answer = int(x ** y)
+        if disp_mode == 'binary':
+            return bin(answer)
+        elif disp_mode == 'hexadecimal':
+            return hex(answer)
+        elif disp_mode == 'octal':
+            return oct(answer)
+        elif disp_mode == 'decimal':
+            return answer
 
     def log(self, x, y):
         return math.log(x, y)
-    
+
     def pie(self, label, slices):
         plabel = label
         pslices = slices
-        plt.pie(pslices, labels=plabel,
-                startangle=90, shadow=True, explode=(0, 0, 0.1, 0),
-                radius=1.5, autopct='%1.2f%%')
-
-
-# add lots more methods to this calculator class.
+        matplotlib.pyplot.pie(pslices, labels=plabel, startangle=90, shadow=True, explode=(0, 0, 0.1, 0), radius=1.5,
+                              autopct='%1.2f%%')
--- main-app.py
+++ main-app.py
@@ -1,4 +1,10 @@
+#import matplotlib.pyplot as plt
+
 from calculator import Calculator
+
+display_modes = ['decimal', 'binary', 'octal', 'hexadecimal']
+val_in_mem = 0
+
 
 def calcMenu():
     print("""
@@ -11,76 +17,209 @@ def calcMenu():
     5) SQUARE
     6) SQUARE ROOT
     7) EXPONENTS
-    8) EXIT
+    8) PIE TIME
+    9) EXIT
     """)
-    choice = int(input("PLEASE ENTER A NUMBER BETWEEN 1 - 8: \n"))
+    choice = int(input("PLEASE ENTER A NUMBER BETWEEN 1 - 9: \n"))
     return choice
 
+
 def getAddNum():
-    a = float(input("Enter the first number that you want to add: \n"))
-    b = float(input("Enter the second number that you want to add: \n"))
+    global val_in_mem
+    a = input("Enter the first number that you want to add: \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
+    b = input("Enter the second number that you want to add: \n")
+    if b == 'mrc':
+        b = float(val_in_mem)
+    else:
+        b = float(b)
+
     return a, b
+
 
 def getSubNum():
-    a = float(input("Enter the first number that you want to subtract: \n"))
-    b = float(input("Enter the second number that you want to subtract: \n"))
+    global val_in_mem
+    a = input("Enter the first number that you want to subtract: \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
+    b = input("Enter the second number that you want to subtract: \n")
+    if b == 'mrc':
+        b = float(val_in_mem)
+    else:
+        b = float(b)
     return a, b
+
 
 def getMultiNum():
-    a = float(input("Enter the first number that you want to multiply: \n"))
-    b = float(input("Enter the second number that you want to multiply: \n"))
+    global val_in_mem
+    a = input("Enter the first number that you want to multiply: \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
+    b = input("Enter the second number that you want to multiply: \n")
+    if b == 'mrc':
+        b = float(val_in_mem)
+    else:
+        b = float(b)
     return a, b
+
 
 def getDivNum():
-    a = float(input("Enter the first number that you want to divide: \n"))
-    b = float(input("Enter the second number that you want to divide: \n"))
+    global val_in_mem
+    a = input("Enter the first number that you want to divide: \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
+    b = input("Enter the second number that you want to divide: \n")
+    if b == 'mrc':
+        b = float(val_in_mem)
+    else:
+        b = float(b)
     return a, b
 
+
 def getSquared():
-    a = float(input("Enter the number that you want to square: \n"))
+    global val_in_mem
+    a = input("Enter the number that you want to square: \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
     return a
+
 
 def getRoot():
-    a = float(input("Enter the number that you want to find the square root of : \n"))
+    global val_in_mem
+    a = input("Enter the number that you want to find the square root of : \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
     return a
 
 
-# def getTwoNumbers():
-#     a = float(input("first number? "))
-#     b = float(input("second number? "))
-#     return a, b
+def getExponent():
+    global val_in_mem
+    a = input("Enter the base number : \n")
+    if a == 'mrc':
+        a = float(val_in_mem)
+    else:
+        a = float(a)
+    b = input("Enter the exponent number : \n")
+    if b == 'mrc':
+        b = float(val_in_mem)
+    else:
+        b = float(b)
+    return a, b
 
+
+def switchDisplayMode(user_choice, prev_mode):
+    if user_choice == 'default':
+        disp_mode = display_modes[prev_mode]
+    else:
+        disp_mode = user_choice
+    return disp_mode
+
+
+def pie(label, slices):
+    plabel = label
+    pslices = slices
+    plt.pie(pslices, labels=plabel, startangle=90, shadow=True, explode=(0, 0, 0.1, 0), radius=1.5,
+            autopct='%1.2f%%')
+    plt.pyplot.legend()
+    plt.pyplot.show()
 
 def displayResult(x: float):
     print(f"Output: {x}", "\n")
 
-
-# def performCalcLoop(calc):
-#     while True:
-#         choice = input("Operation? ")
-#         if choice == 'q':
-#             break  # user types q to quit calulator.
-#         elif choice == 'add':
-#             a, b = getAddNum()
-#             displayResult(calc.add(a, b))
-#         else:
-#             print("That is not a valid input.")
-
 def performCalcLoop(calc):
-    choice = "" #Set choice to a value so it can meet the condition below and start the loop
-    while choice != 8:
+    global val_in_mem
+    subject_list = []
+    percent_list = []
+    disp_prev_num = 0
+    disp_ask = input("Set a display mode?\n")
+    if disp_ask == '' or disp_ask == 'n':
+        disp_inpt = 'default'
+    elif disp_ask == 'y':
+        disp_inpt = input("Which mode" + str(display_modes))
+    choice = ""  # Set choice to a value so it can meet the condition below and start the loop
+    while choice != 9:
+        if disp_prev_num > 3:
+            disp_prev_num = 0
+        disp_mode = switchDisplayMode(disp_inpt, disp_prev_num)
         choice = calcMenu()
         if choice == 1:
+            print("Add")
             a, b = getAddNum()
-            displayResult(calc.add(a, b))
+            result = calc.add(a, b, disp_mode)
+            displayResult(result)
         elif choice == 2:
-            pass
+            print("Subtract")
+            a, b = getSubNum()
+            displayResult(calc.sub(a, b, disp_mode))
         elif choice == 3:
             print("Multiply")
-        elif choice == 8:
+            a, b = getMultiNum()
+            displayResult(calc.mul(a, b, disp_mode))
+        elif choice == 4:
+            print("Divide")
+            a, b = getDivNum()
+            displayResult(calc.div(a, b, disp_mode))
+        elif choice == 5:
+            print("Square")
+            a, b = getSquared()
+            displayResult(calc.squ(a, disp_mode))
+        elif choice == 6:
+            print("Square Root")
+            a = getRoot()
+            displayResult(calc.roo(a, disp_mode))
+        elif choice == 7:
+            print("Exponent")
+            a, b = getExponent()
+            displayResult(calc.exp(a, b, disp_mode))
+        elif choice == 9:
             print("Calculator is now OFF")
+        # elif choice == 8:
+        #     print("Lets make a pie chart!")
+        #     subcount = int(input("How many subjects are on your pie chart?\n"))
+        #     for x in range(0, subcount):
+        #         sub = str(input("Please enter your subject\n"))
+        #         subject_list.append(sub)
+        #
+        #     # percentage = input(input("How much of each on your pie?\n"))
+        #     for x in range(0, subcount):
+        #         percent = input("Please enter the percentage for each subject\n")
+        #         percent_list.append(percent)
+        #     print("Please wait....creating pie")
+        #     pie(subject_list, percent_list)
         else:
-            print("Invalid choice: Please enter a number between 1 - 8\n\n")
+            print("Invalid choice: Please enter a number between 1 - 9\n\n")
+        disp_prev_num += 1
+        user_inpt = input()
+        if user_inpt == 'm+':
+            val_in_mem = result
+            print(result)
+        if user_inpt == 'mc':
+            val_in_mem = 0
+
+
+# main start
+def main():
+    calc = Calculator()
+    performCalcLoop(calc)
+    print("Done Calculating.")
+
+
+if __name__ == '__main__':
+    main()
+
 
 # main start
 def main():
